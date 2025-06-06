5677f4d3fe0f0f683685c3a8e8ccbdf581f0d404
jjangminii <wjdals030328@catholic.ac.kr>
2025-06-06 18:09:40 +0900

    init: 토큰 설정

---
+++ src/styles/global.css.ts
@@ -0,0 +1,17 @@
+import { globalStyle } from '@vanilla-extract/css';
+
+
+
+/* HTML & Body Styles */
+globalStyle('html, body', {
+  fontSize: '62.5%',
+  fontFamily: '"Paperlogy", sans-serif',
+  scrollbarWidth: 'none',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  scrollBehavior: 'smooth',
+  background: '#0F101B',
+  margin: '0',
+  padding: '0',
+});
---
+++ src/styles/theme.css.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@vanilla-extract/css';
+import { createSprinkles, defineProperties } from '@vanilla-extract/sprinkles';
+import { color } from './tokens/color.css';
+import { typography } from './tokens/typography.css';
+
+const tokens = {
+   color: color,
+   ...typography,
+};
+
+const properties = defineProperties({
+   properties: tokens,
+});
+
+const sprinkles = createSprinkles(properties);
+const [themeClass, themeVars] = createTheme(tokens);
+
+export { sprinkles, themeClass, themeVars };
